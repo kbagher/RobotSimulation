2bdf95b4be92f3d6fda425c7b0b907a628a23a76
Kassem Bagher <kassem@waqood.sa>
2017-03-29 11:00:45 +1100

    Added new max method for arrays and lists

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,20 +1,38 @@
-class MyMath
-{
-    public static int max(int a, int b, int c)
-    {
-        return Math.max(Math.max(a,b),c);
-    }
-    public static int max(int a, int b, int c, int d)
-    {
-        return Math.max(Math.max(Math.max(a,b),c),d);
-    }
-    public static int max(int a, int b, int c, int d,int e)
-    {
-        return Math.max(Math.max(Math.max(Math.max(a,b),c),d), e);
-    }
-    public static int max(int a, int b, int c, int d,int e,int f)
-    {
-        return Math.max(Math.max(Math.max(Math.max(Math.max(a,b),c),d), e), f);
-    }
-}
 
+import java.util.*;
+
+class MyMath {
+	public static int max(int a, int b, int c) {
+		return Math.max(Math.max(a, b), c);
+	}
+
+	public static int max(int a, int b, int c, int d) {
+		return Math.max(Math.max(Math.max(a, b), c), d);
+	}
+
+	public static int max(int a, int b, int c, int d, int e) {
+		return Math.max(Math.max(Math.max(Math.max(a, b), c), d), e);
+	}
+
+	public static int max(int a, int b, int c, int d, int e, int f) {
+		return Math.max(Math.max(Math.max(Math.max(Math.max(a, b), c), d), e), f);
+	}
+
+	public static int max(int srcArray[]) {
+		int maxValue = 0;
+		for (int i : srcArray) {
+			if (i > maxValue)
+				maxValue = i;
+		}
+		return maxValue;
+	}
+
+	public static int max(ArrayList<Integer> srcList) {
+		int maxValue = 0;
+		for (Integer i : srcList) {
+			if (i > maxValue)
+				maxValue = i;
+		}
+		return maxValue;
+	}
+}
